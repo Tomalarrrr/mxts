8920a4263388aee8be6482482f74de81dc8395d8
Tomalarrrr <105936702+Tomalarrrr@users.noreply.github.com>
2022-09-08 14:43:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -346,6 +346,18 @@ class Slideshow {
 })();
 
 
-/*landing page*/
+/*Ligths off Display vid*/
+const buttonEl = document.querySelector('.button');
+const overlayEl = document.querySelector('.overlay');
+
+const turnOn = () => {
+    overlayEl.style.display = 'block'
+}
+buttonEl.addEventListener('click', turnOn);
+
+const turnOff = (e) => {
+    if (e.target.matches('.overlay')) overlayEl.style.display = 'none';
+}
+window.addEventListener('click', turnOff);
 
 
